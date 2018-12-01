3901c661d26ff989108c8b80b89ba3cf311a23bb
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-01 11:53:33 -0800

    changed servo intake control from bumpers to dpad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -43,24 +43,36 @@ public class DriverControlled extends OpMode{
         boolean G2RB = gamepad2.right_bumper;
         boolean G1LB = gamepad1.left_bumper;
         boolean G1RB = gamepad1.right_bumper;
+        boolean G2DPADup = gamepad2.dpad_up;
+        boolean G2DPADdown = gamepad2.dpad_down;
+
 
 
 
         //robot movement
-        robot.frontLeftMotor.setPower(Y + X + R);
-        robot.backLeftMotor.setPower(Y - X + R);
-        robot.frontRightMotor.setPower(-Y + X + R);
-        robot.backRightMotor.setPower(-Y -X + R);
+        robot.frontLeftMotor.setPower(Y + X - R);
+        robot.backLeftMotor.setPower(Y - X - R);
+        robot.frontRightMotor.setPower(-Y + X - R);
+        robot.backRightMotor.setPower(-Y -X - R);
 
 
 
         // code for intake of minerals via servo
-        if(G2RB)
+        if(G2DPADdown)
             robot.servoIntake.setPower(1.0);
-        else if(G2LB)
+        else if(G2DPADup)
             robot.servoIntake.setPower(-1.0);
         else
             robot.servoIntake.setPower(0.0);
+
+
+        //movement of secondary motor in middle of arm ?? recomment this
+        if(G2LB)
+            robot.middleArmMotor.setPower(1.0);
+        else if(G2RB)
+            robot.middleArmMotor.setPower(-1.0);
+        else
+            robot.middleArmMotor.setPower(0.0);
 
 
         //movement of main arm motor
