b073a3b3afa862bcc53daa39fbd407b492fb12a6
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-01 17:48:27 -0800

    removed all instances of the vertical and horizontal arm variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -12,11 +12,10 @@ public class RobotHardware
     public DcMotor frontRightMotor;
     public DcMotor backLeftMotor;
     public DcMotor backRightMotor;
-    //public DcMotor verticalArmMovement;
-    //public DcMotor horizontalArmMovement;
     public Servo   servoArm;
     public CRServo servoIntake;
     public DcMotor armMotor;
+    public DcMotor middleArmMotor;
 
     // creating the hardware map
     HardwareMap hardwareMap;
@@ -32,19 +31,18 @@ public class RobotHardware
         frontRightMotor = hardwareMap.get(DcMotor.class, "front right motor");
         backLeftMotor = hardwareMap.get(DcMotor.class, "back left motor");
         backRightMotor = hardwareMap.get(DcMotor.class, "back right motor");
-        //verticalArmMovement = hardwareMap.get(DcMotor.class, "vertical movement");
-        //horizontalArmMovement = hardwareMap.get(DcMotor.class, "horizontal movement");
         //servoArm = hardwareMap.get(Servo.class, "servo arm");
         servoIntake = hardwareMap.get(CRServo.class, "servo intake");
         armMotor = hardwareMap.get(DcMotor.class, "arm motor");
+        middleArmMotor = hardwareMap.get(DcMotor.class, "middle arm motor");
 
         // set motor power
         frontLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backLeftMotor.setPower(0);
         backRightMotor.setPower(0);
-        //verticalArmMovement.setPower(0);
-        //horizontalArmMovement.setPower(0);
+        middleArmMotor.setPower(0);
+
 
         // set motor mode
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //...RunMode.RUN_TO_POSITION - for autonomous when you want to use encoder positions
@@ -52,16 +50,17 @@ public class RobotHardware
         backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //verticalArmMovement.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //horizontalArmMovement.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        middleArmMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
 
         // set motor zeroPowerBehavior
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); //...zeroPowerBehavior.float we want it to stop exactly when set to zero
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //verticalArmMovement.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //horizontalArmMovement.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        middleArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         /* set servo position
         servoArm.setPosition(0); //have to change later when robot is running ***
@@ -70,13 +69,6 @@ public class RobotHardware
 
     }
 
-    /*public void forwardPositions(){
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-    }
-    */
  }
 
