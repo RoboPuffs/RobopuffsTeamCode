45e5518b12bdcfbbe6650c3fc4afdfaf6c66ad66
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-02 19:56:44 -0800

    created opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -1,12 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name="extreme meme", group="Autonomous")
-public class TestAutonomous {
+public class TestAutonomous extends OpMode {
 
+    RobotHardware robot = new RobotHardware();
 
+    @Override
+    public void init() {
+        telemetry.addData("STATUS", "Beginning Initialization");
+        robot.init(hardwareMap);
+        telemetry.addData("STATUS", "Initialized");
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
 
 
 
