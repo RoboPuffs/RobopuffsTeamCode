052e03210ccc36ee9d58e1f98c603164ec054e2f
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-02 19:39:38 -0800

    fixed a line of code with improper use of semicolon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous(name="Autonomous1", group="Autonomous")
 public class TestAutonomous {
+
+
+
+
+
 }
+
+
