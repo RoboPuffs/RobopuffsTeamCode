ca1326850a1ec567a0ac35716956def35b7bcafc
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-02 21:11:24 -0800

    cleaned DriverControlled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -13,8 +13,6 @@ public class DriverControlled extends OpMode{
     RobotHardware robot   = new RobotHardware();
 
 
-
-
     //run once on init()
     @Override
     public void init() {
@@ -39,55 +37,42 @@ public class DriverControlled extends OpMode{
         double Y = gamepad1.right_stick_y;
         double X = gamepad1.right_stick_x;
         double R = gamepad1.left_stick_x;
-        boolean G2LB = gamepad2.left_bumper;
-        boolean G2RB = gamepad2.right_bumper;
-        boolean G1LB = gamepad1.left_bumper;
-        boolean G1RB = gamepad1.right_bumper;
-        boolean G2DPADup = gamepad2.dpad_up;
-        boolean G2DPADdown = gamepad2.dpad_down;
-
-
-
+        boolean g2_LB = gamepad2.left_bumper;
+        boolean g2_RB = gamepad2.right_bumper;
+        boolean g1_LB = gamepad1.left_bumper;
+        boolean g1_RB = gamepad1.right_bumper;
+        boolean g2_DPADup = gamepad2.dpad_up;
+        boolean g2_DPADdown = gamepad2.dpad_down;
 
         //robot movement
         robot.frontLeftMotor.setPower(Y + X - R);
         robot.backLeftMotor.setPower(Y - X - R);
         robot.frontRightMotor.setPower(-Y + X - R);
-        robot.backRightMotor.setPower(-Y -X - R);
-
-
+        robot.backRightMotor.setPower(-Y - X - R);
 
         // code for intake of minerals via servo
-        if(G2DPADdown)
+        if(g2_DPADdown)
             robot.servoIntake.setPower(1.0);
-        else if(G2DPADup)
+        else if(g2_DPADup)
             robot.servoIntake.setPower(-1.0);
         else
             robot.servoIntake.setPower(0.0);
 
-
         //movement of secondary motor in middle of arm ?? recomment this
-        if(G2LB)
+        if(g2_LB)
             robot.middleArmMotor.setPower(1.0);
-        else if(G2RB)
+        else if(g2_RB)
             robot.middleArmMotor.setPower(-1.0);
         else
             robot.middleArmMotor.setPower(0.0);
 
-
         //movement of main arm motor
-        if(G1LB)
+        if(g1_LB)
             robot.armMotor.setPower(1.0);
-        else if(G1RB)
+        else if(g1_RB)
             robot.armMotor.setPower(-1.0);
         else
             robot.armMotor.setPower(0);
-
-
-
-
-
-
 
     }
 
