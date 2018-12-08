80f8acc576936042d8abf8c42fc0e2f6762a6797
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-08 15:40:48 -0800

    minor edits for color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorAutonomous.java
@@ -1,54 +1,66 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-@Autonomous(name = "color sensor test", group = "Autonomous")
+@TeleOp(name = "color sensor test", group = "TeleOp")
 public class ColorSensorAutonomous extends OpMode {
 
-    public RobotHardware Robot = new RobotHardware();
+    RobotHardware robot = new RobotHardware();
     public int cBlueValue, cRedValue, cGreenValue;
 
-    public void forwardPosition(){
-        Robot.frontLeftMotor.setPower(.5);
-        Robot.frontRightMotor.setPower(-.5);
-        Robot.backLeftMotor.setPower(.5);
-        Robot.backRightMotor.setPower(-.5);
+    /*
+    private void moveForward(){
+        robot.frontLeftMotor.setPower(.5);
+        robot.frontRightMotor.setPower(-.5);
+        robot.backLeftMotor.setPower(.5);
+        robot.backRightMotor.setPower(-.5);
 
     }
+    */
 
     @Override
-    public void init(){
+    public void init() {
+
         telemetry.addData("STATUS", "Beginning Initialization");
-        Robot.init(hardwareMap);
 
         //have to change to color values for yellow mineral
         cBlueValue = 255;
         cRedValue = 205;
         cGreenValue = 53;
 
-
         telemetry.addData("STATUS", "Initialized");
 
+        robot.init(hardwareMap);
+
     }
 
+    // loop on init()
     @Override
     public void init_loop() {
-
     }
 
+    // code that runs once on start()
     @Override
     public void start() {
-
     }
 
+    // loops on start(), main opMode
     @Override
     public void loop() {
 
+        //detecting minerals with color sensor
+        if(robot.colorSensor.blue() <= cBlueValue && robot.colorSensor.red() >= cRedValue && robot.colorSensor.green() >= cGreenValue) {
+
+            telemetry.addData("Mineral", "Yellow");
+        }
+        else {
+            telemetry.addData("Mineral", "Not Yellow");
+
+        }
+
     }
-
-
 
 
 }
