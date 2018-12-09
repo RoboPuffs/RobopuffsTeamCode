eb1771fc880719089b2674a32b3f267bf467470b
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-09 10:10:29 -0800

    fixed servo positions, changed gamepad controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -38,51 +38,43 @@ public class DriverControlled extends OpMode{
         //defining gamepad variables
         //gamepad 1 will be used solely for robot movement
         //g1 stands for controls on gamepad 1
-        double Y = gamepad1.right_stick_y;
-        double X = gamepad1.right_stick_x;
-        double R = gamepad1.left_stick_x;
+        double g1_rightstick_y = gamepad1.right_stick_y;
+        double g1_rightsick_x = gamepad1.right_stick_x;
+        double g1_leftstick_x = gamepad1.left_stick_x;
 
         //gamepad 2 will be used solely for mineral mechanism
         //g2 stands for controls on gamepad 2
-        boolean g2_LB = gamepad2.left_bumper;
-        boolean g2_RB = gamepad2.right_bumper;
-        boolean g2_DPAD_right = gamepad2.dpad_right;
-        boolean g2_DPAD_left = gamepad2.dpad_left;
-        boolean g2_DPAD_up = gamepad2.dpad_up;
-        boolean g2_DPAD_down = gamepad2.dpad_down;
+        boolean g2_LB = gamepad2.left_bumper; //for mineral intake
+        boolean g2_RB = gamepad2.right_bumper; //for servo emptying minerals at end of arm
+        boolean g2_DPAD_right = gamepad2.dpad_right; //for emptying minerals into mineral compartment
+        boolean g2_DPAD_up = gamepad2.dpad_up; //for moving arm to put minerals into lander
+        boolean g2_DPAD_down = gamepad2.dpad_down; //for reversing arm to come back to original position
 
+        final double SERVO_EMPTIED_POSITION = .8;  //position value for servo emptying minerals position
+        final double SERVO_HOLDING_POSITION = 0.0; //position value for servo in holding minerals position
+        final double MINERAL_HOLDING_START_POSITION= 0.0;
 
-        //position value for servo emptying minerals position
-        final double SERVO_EMPTIED_POSITION = .8;
-        //position value for servo in holding minerals position
-        final double SERVO_HOLDING_POSITION = 0.0;
 
         //robot movement
-        robot.frontLeftMotor.setPower(Y + X - R);
-        robot.backLeftMotor.setPower(Y - X - R);
-        robot.frontRightMotor.setPower(-Y + X - R);
-        robot.backRightMotor.setPower(-Y - X - R);
+        robot.frontLeftMotor.setPower(g1_rightstick_y + g1_rightsick_x - g1_leftstick_x);
+        robot.backLeftMotor.setPower(g1_rightstick_y - g1_rightsick_x - g1_leftstick_x);
+        robot.frontRightMotor.setPower(-g1_rightstick_y + g1_rightsick_x - g1_leftstick_x);
+        robot.backRightMotor.setPower(-g1_rightstick_y - g1_rightsick_x - g1_leftstick_x);
 
         /*code for intake of minerals via motor
         if(g2_LB)
             robot.mineralIntakeMotor.setPower(1.0);
         else
             robot.mineralIntakeMotor.setPower(0.0);
-          */
-
-        //will use right bumper to empty minerals into mineral holder
-        /*and right trigger to move original mineral holder back to intake position
-        if(g2_DPAD_right)
-            robot.mineralOutputMotor.setPower(.25);
-        else if(g2_DPAD_left)
-            robot.mineralOutputMotor.setPower(-.25);
-        else
-            robot.mineralOutputMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    */
+        */
 
 
+        robot.mineralOutputServo.setPosition(MINERAL_HOLDING_START_POSITION);
+
+
+        //using up and down buttons on dpad to move main arm motor on back of robot
         if(g2_DPAD_up)
-            robot.mainArmJointMotor.setPower(.4);
+            robot.mainArmJointMotor.setPower(.6);
         else if(g2_DPAD_down)
             robot.mainArmJointMotor.setPower(-.4);
         else {
@@ -90,19 +82,17 @@ public class DriverControlled extends OpMode{
             robot.mainArmJointMotor.setPower(0);
         }
 
-
         //right bumper will move servo to empty out stored minerals
         if(g2_RB)
             robot.mineralHolderArmServo.setPosition(SERVO_EMPTIED_POSITION);
         else
             robot.mineralHolderArmServo.setPosition(SERVO_HOLDING_POSITION);
 
-
     }
 
     // runs once
     @Override
     public void stop() {
-        //could set servo arm back to zero etc.
+
     }
 }
