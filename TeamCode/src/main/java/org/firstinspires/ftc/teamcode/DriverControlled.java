e1313ed13ce474ae886d8ada7885bf62e9faebd7
shaydamoezzi <shaydamoezzi@gmail.com>
2018-12-09 07:26:21 -0800

    added code for 2 motors and one servo for mineral arm mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-@TeleOp(name="DriverControlled", group="TeleOp")
+@TeleOp(name="Driver Controlled", group="TeleOp")
 
 public class DriverControlled extends OpMode{
 
@@ -16,6 +16,7 @@ public class DriverControlled extends OpMode{
     //run once on init()
     @Override
     public void init() {
+        telemetry.addData("STATUS", "Beginning Initialization");
         robot.init(hardwareMap);
         telemetry.addData("STATUS", "Initialized");
     }
@@ -34,15 +35,27 @@ public class DriverControlled extends OpMode{
     @Override
     public void loop() {
 
+        //defining gamepad variables
+        //gamepad 1 will be used solely for robot movement
+        //g1 stands for controls on gamepad 1
         double Y = gamepad1.right_stick_y;
         double X = gamepad1.right_stick_x;
         double R = gamepad1.left_stick_x;
+
+        //gamepad 2 will be used solely for mineral mechanism
+        //g2 stands for controls on gamepad 2
         boolean g2_LB = gamepad2.left_bumper;
         boolean g2_RB = gamepad2.right_bumper;
-        boolean g1_LB = gamepad1.left_bumper;
-        boolean g1_RB = gamepad1.right_bumper;
-        boolean g2_DPADup = gamepad2.dpad_up;
-        boolean g2_DPADdown = gamepad2.dpad_down;
+        boolean g2_DPAD_right = gamepad2.dpad_right;
+        boolean g2_DPAD_left = gamepad2.dpad_left;
+        boolean g2_DPAD_up = gamepad2.dpad_up;
+        boolean g2_DPAD_down = gamepad2.dpad_down;
+
+
+        //position value for servo emptying minerals position
+        final double SERVO_EMPTIED_POSITION = .8;
+        //position value for servo in holding minerals position
+        final double SERVO_HOLDING_POSITION = 0.0;
 
         //robot movement
         robot.frontLeftMotor.setPower(Y + X - R);
@@ -50,29 +63,40 @@ public class DriverControlled extends OpMode{
         robot.frontRightMotor.setPower(-Y + X - R);
         robot.backRightMotor.setPower(-Y - X - R);
 
-        // code for intake of minerals via servo
-        if(g2_DPADdown)
-            robot.servoIntake.setPower(1.0);
-        else if(g2_DPADup)
-            robot.servoIntake.setPower(-1.0);
-        else
-            robot.servoIntake.setPower(0.0);
-
-        //movement of secondary motor in middle of arm ?? recomment this
+        /*code for intake of minerals via motor
         if(g2_LB)
-            robot.middleArmMotor.setPower(1.0);
-        else if(g2_RB)
-            robot.middleArmMotor.setPower(-1.0);
+            robot.mineralIntakeMotor.setPower(1.0);
         else
-            robot.middleArmMotor.setPower(0.0);
+            robot.mineralIntakeMotor.setPower(0.0);
+          */
 
-        //movement of main arm motor
-        if(g1_LB)
-            robot.armMotor.setPower(1.0);
-        else if(g1_RB)
-            robot.armMotor.setPower(-1.0);
+        //will use right bumper to empty minerals into mineral holder
+        /*and right trigger to move original mineral holder back to intake position
+        if(g2_DPAD_right)
+            robot.mineralOutputMotor.setPower(.25);
+        else if(g2_DPAD_left)
+            robot.mineralOutputMotor.setPower(-.25);
         else
-            robot.armMotor.setPower(0);
+            robot.mineralOutputMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    */
+
+
+        if(g2_DPAD_up)
+            robot.mainArmJointMotor.setPower(.4);
+        else if(g2_DPAD_down)
+            robot.mainArmJointMotor.setPower(-.4);
+        else {
+            robot.mainArmJointMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.mainArmJointMotor.setPower(0);
+        }
+
+
+        //right bumper will move servo to empty out stored minerals
+        if(g2_RB)
+            robot.mineralHolderArmServo.setPosition(SERVO_EMPTIED_POSITION);
+        else
+            robot.mineralHolderArmServo.setPosition(SERVO_HOLDING_POSITION);
+
 
     }
 
